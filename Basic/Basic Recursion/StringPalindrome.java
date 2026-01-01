88ce51cff65e9b1b9bba807adcd167e95216bec8
Vansh2693 <vanshraj2693@gmail.com>
2026-01-01 23:58:10 +0530

    Modified String Palindrome pgm

--- Basic/Basic Recursion/StringPalindrome.java
+++ Basic/Basic Recursion/StringPalindrome.java
@@ -1,15 +1,15 @@
 public class StringPalindrome {   
-    public static boolean palindromeCheck(String s) {
+    public static boolean palindromeCheck(String str) {
         //your code goes here
-        return palindromeHelper(s,0,s.length()-1);
+        return palindromeHelper(str,0,str.length()-1);
     }
 
     private static boolean palindromeHelper(String s, int low, int high){
         if(low>=high) return true;
 
-        if(s.charAt(low)!=s.charAt(high)) return false;
+        if(str.charAt(low)!=str.charAt(high)) return false;
 
-        return palindromeHelper(s,low+1, high-1);
+        return palindromeHelper(str,low+1, high-1);
     }
 
     public static void main(String[] args) {
